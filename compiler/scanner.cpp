6bb6d5c605f5bab29d029185127f8395012e0d86
Evan Wild <evan@evanwild.com>
2024-01-24 19:47:34 -0500

    fix scanning html strings

--- compiler/scanner.cpp
+++ compiler/scanner.cpp
@@ -3,15 +3,11 @@
 #include <cctype>
 #include <cstddef>
 #include <format>
+#include <map>
 #include <stdexcept>
 #include <string>
-#include <unordered_map>
 #include <utility>
 #include <vector>
-
-const std::unordered_map<std::string, TokenKind> keywords = {
-    {"comp", TokenKind::Component},
-};
 
 Scanner::Scanner(std::string source)
     : m_source{std::move(source)}, m_index{0} {}
@@ -30,60 +26,26 @@ char Scanner::consume() {
     return '\0';
 }
 
+const std::map<std::string, TokenKind> keywords = {
+    {"comp", TokenKind::Component},
+};
+
 std::vector<Token> Scanner::scan_tokens() {
     std::vector<Token> result;
+    int num_open = 0;
 
-    while (m_index < m_source.length()) {
-        // No whitespace allowed between "<" or "</" and tag name
-        if (!result.empty() && std::isspace(peek()) &&
-            (result.back().kind == TokenKind::Open ||
-             result.back().kind == TokenKind::OpenSlash)) {
-            const auto error_msg =
-                std::format("Scanning error: No whitespace can be after \"{}\"",
-                            result.back().lexeme);
-            throw std::runtime_error(error_msg);
-        }
-
-        // Check if we should read an HTML string literal
-        if (m_index > 0 && m_source[m_index - 1] == '>') {
-            if (m_source.find('<', m_index) != std::string::npos) {
-                std::string lexeme;
-                while (peek() != '<') {
-                    lexeme += consume();
-                }
-
-                result.emplace_back(TokenKind::String, std::move(lexeme));
-                continue;
-            }
-        }
-
+    while (m_index < m_source.size()) {
         const char ch = consume();
 
         if (std::isspace(ch)) {
             continue;
-        } else if (ch == '<' && peek() == '/') {
-            consume();
-            result.emplace_back(TokenKind::OpenSlash, "</");
-        } else if (ch == '/' && peek() == '>') {
-            consume();
-            result.emplace_back(TokenKind::SlashClose, "/>");
-        } else if (ch == '(') {
-            result.emplace_back(TokenKind::LParen, "(");
-        } else if (ch == ')') {
-            result.emplace_back(TokenKind::RParen, ")");
-        } else if (ch == '{') {
-            result.emplace_back(TokenKind::LCurly, "{");
-        } else if (ch == '}') {
-            result.emplace_back(TokenKind::RCurly, "}");
-        } else if (ch == '<') {
-            result.emplace_back(TokenKind::Open, "<");
-        } else if (ch == '>') {
-            result.emplace_back(TokenKind::Close, ">");
-        } else if (std::isalpha(ch)) {
+        }
+
+        if (std::isalpha(ch)) {
             TokenKind kind;
             std::string lexeme;
-            lexeme += ch;
 
+            lexeme += ch;
             while (isalnum(peek())) {
                 lexeme += consume();
             }
@@ -95,6 +57,58 @@ std::vector<Token> Scanner::scan_tokens() {
             }
 
             result.emplace_back(kind, std::move(lexeme));
+            continue;
+        }
+
+        if (ch == '<') {
+            if (peek() == '/') {
+                consume();
+                result.emplace_back(TokenKind::OpenSlash, "</");
+                --num_open;
+            } else {
+                result.emplace_back(TokenKind::Open, "<");
+                ++num_open;
+            }
+
+            if (isspace(peek())) {
+                throw std::runtime_error(
+                    "Scanning error: No whitespace allowed after < or </");
+            }
+            continue;
+        }
+
+        auto scan_string = [&]() {
+            if (num_open > 0) {
+                std::string lexeme;
+                while (m_index < m_source.size() && peek() != '<') {
+                    lexeme += consume();
+                }
+                result.emplace_back(TokenKind::String, std::move(lexeme));
+            }
+        };
+
+        if (ch == '>') {
+            result.emplace_back(TokenKind::Close, ">");
+            scan_string();
+            continue;
+        }
+
+        if (ch == '/' && peek() == '>') {
+            consume();
+            result.emplace_back(TokenKind::SlashClose, "/>");
+            --num_open;
+            scan_string();
+            continue;
+        }
+
+        if (ch == '(') {
+            result.emplace_back(TokenKind::LParen, "(");
+        } else if (ch == ')') {
+            result.emplace_back(TokenKind::RParen, ")");
+        } else if (ch == '{') {
+            result.emplace_back(TokenKind::LCurly, "{");
+        } else if (ch == '}') {
+            result.emplace_back(TokenKind::RCurly, "}");
         } else {
             const auto error_msg =
                 std::format("Scanning error: Did not expect \'{}\'", ch);
